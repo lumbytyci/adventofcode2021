521ce403cfd6810e3cd0e154803301c52a5ce84f
Lumi Bytyci <lumbytyci@hotmail.com>
2021-12-09 12:42:19 +0100

    Add day7

---
+++ day7/Part1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AOC2021_Day6
+{
+    public static class Part1
+    {
+        public static void Main(string[] args)
+        {
+            var input = "16,1,2,0,4,2,7,1,2,14";
+
+            /* Run with sample input if any argument is supplied */
+            if (args.Length == 1)
+            {
+                input = File.ReadAllText("./input.txt");
+            }
+
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            var result = RunPart(input);
+            Console.WriteLine($"Result: {result}. Execution time: {watch.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+
+        public static int RunPart(string input)
+        {
+            var levels = input.Split(",").Select(int.Parse).ToList();
+
+            var lowestEnergy = int.MaxValue;
+            for (var i = levels.Min(); i < levels.Max() + 1; i++)
+            {
+                var energy = 0;
+                foreach(var level in levels)
+                {
+                    energy += Math.Abs(level - i);
+                }
+
+                if (energy < lowestEnergy) lowestEnergy = energy;
+            }
+
+            return lowestEnergy;
+        }
+    }
+}
---
+++ day7/Part2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AOC2021_Day6
+{
+    public static class Part2
+    {
+        public static void Main(string[] args)
+        {
+            var input = "16,1,2,0,4,2,7,1,2,14";
+
+            /* Run with sample input if any argument is supplied */
+            if (args.Length == 1)
+            {
+                input = File.ReadAllText("./input.txt");
+            }
+
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            var result = RunPart(input);
+            Console.WriteLine($"Result: {result}. Execution time: {watch.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+
+        public static int RunPart(string input)
+        {
+            var levels = input.Split(",").Select(int.Parse).ToList();
+
+            var lowestEnergy = int.MaxValue;
+            for (var i = levels.Min(); i < levels.Max() + 1; i++)
+            {
+                var energy = 0;
+                foreach (var level in levels)
+                {
+                    var delta = Math.Abs(level - i);
+                    energy += delta + (delta * (delta - 1) / 2);
+                }
+
+                if (energy < lowestEnergy) lowestEnergy = energy;
+            }
+
+            return lowestEnergy;
+        }
+    }
+}
